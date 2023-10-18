4c710e5567cb3844c7828e4c3534339b8872101e
naddicott-dtech <naddicott@dtechhs.org>
2023-10-18 15:15:44 -0700

    some movement in the cell membrane (still buggy)

--- Scenes/Main/Player.gd
+++ Scenes/Main/Player.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
-var amplitude = 10.0
-var frequency = 1.0
+var amplitude = 25.0
+var target_amplitude = 50.0
+var amplitude_change_speed = 0.5 # Adjust this for faster or slower transitions
+var frequency = 1.5
+var target_frequency = 2 # This can be adjusted based on your desired effect
+var frequency_change_speed = 0.2 # Adjust this for faster or slower transitions
 var time_passed = 0.0
 var osc_speed = 0.5
-var noise = OpenSimplexNoise.new() # Godot has built-in Simplex noise                                                                            
+var noise = OpenSimplexNoise.new() # Godot has built-in Simplex noise  
+var noise_period = 0.5
+var target_noise_period = 0.8
+var noise_period_change_speed = 0.05 # Adjust this for faster or slower transitions
+																		  
 # Declare curve at class scope
 var curve: Curve2D = null
 
@@ -18,11 +26,11 @@ func add_wavy_points(curve, point1, point2, num_intermediate_points, time):
 		var wavy_point = base_point + offset
 		curve.add_point(wavy_point)
 
-#Hi github, I still work!
 func _ready():
 	# Access the AmoebaShape (Path2D) node
 	var amoeba_shape = get_node("AmoebaShape")  # Adjusted the path
 	self.z_index = 1  # Set to a higher value than Moving Background
+	noise.set_period(noise_period)
 	
 	# Initialize the Curve2D resource
 	curve = Curve2D.new()
@@ -66,6 +74,40 @@ func _ready():
 func _process(delta): 
 	time_passed += delta
 	var amoeba_shape = get_node("AmoebaShape")  # Adjusted the path
+	
+	# Adjust amplitude towards target
+	if amplitude < target_amplitude:
+		amplitude += amplitude_change_speed * delta
+		if amplitude > target_amplitude:
+			amplitude = target_amplitude
+	elif amplitude > target_amplitude:
+		amplitude -= amplitude_change_speed * delta
+		if amplitude < target_amplitude:
+			amplitude = target_amplitude
+
+	# Adjust frequency towards target
+	if frequency < target_frequency:
+		frequency += frequency_change_speed * delta
+		if frequency > target_frequency:
+			frequency = target_frequency
+	elif frequency > target_frequency:
+		frequency -= frequency_change_speed * delta
+		if frequency < target_frequency:
+			frequency = target_frequency
+			
+	# Adjust noise period towards target
+	if noise_period < target_noise_period:
+		noise_period += noise_period_change_speed * delta
+		if noise_period > target_noise_period:
+			noise_period = target_noise_period
+	elif noise_period > target_noise_period:
+		noise_period -= noise_period_change_speed * delta
+		if noise_period < target_noise_period:
+			noise_period = target_noise_period
+
+	noise.set_period(noise_period)
+
+
 	# Clear the existing points from the curve and re-add them with the new undulation
 	curve.clear_points()
 	var control_point1 = amoeba_shape.get_node("ControlPoint1").position
@@ -82,9 +124,24 @@ func _process(delta):
 	curve.add_point(control_point2)
 	add_wavy_points(curve, control_point2, control_point3, 3, time_passed)
 	curve.add_point(control_point3)
+	add_wavy_points(curve, control_point3, control_point4, 3, time_passed)
+	curve.add_point(control_point4)
+	add_wavy_points(curve, control_point4, control_point5, 3, time_passed)
+	curve.add_point(control_point5)
+	add_wavy_points(curve, control_point5, control_point6, 3, time_passed)
+	curve.add_point(control_point6)
+	add_wavy_points(curve, control_point6, control_point7, 3, time_passed)
+	curve.add_point(control_point7)
+	add_wavy_points(curve, control_point7, control_point8, 3, time_passed)
+	curve.add_point(control_point8)
+	add_wavy_points(curve, control_point8, control_point1, 3, time_passed)
+	curve.add_point(control_point1)
+	# Trigger the _draw() method to render the updated curve
+	update()
+
 
 func _draw():
-	print("Entering _draw function")  # Debug message
+	#print("Entering _draw function")  # Debug message
 	var amoeba_shape = $AmoebaShape  # Adjusted the path
 	var curve = amoeba_shape.curve
 	#var length = curve.get_baked_length()
@@ -103,4 +160,4 @@ func _draw():
 		#print("Drawing line from ", prev_point, " to ", point)  # Debug message
 
 	draw_polyline(points, Color(0, 0, 0), 5)
-	print("Exiting _draw function")  # Debug message
+	#print("Exiting _draw function")  # Debug message
